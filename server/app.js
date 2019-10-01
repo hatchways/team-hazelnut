025c17c12c31db4dacc29297c277664343db3b65
Pushpam Choudhary <42444413+PC25@users.noreply.github.com>
2019-10-01 14:31:01 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // error rendere
   res.status(err.status || 500);
   res.json({ error: err });
 });
