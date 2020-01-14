0e485e8a298b3739dcf964fb2beeb44a05bd8f97
CometS1 <sppang1@yahoo.com>
2020-01-14 11:26:14 -0500

    Removed ping from project

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ import logger from "morgan";
 import passport from "passport";
 import cors from "cors";
 
-import dbConnection from "./db/mongoose";
 import indexRouter from "./routes/index";
 import profileRouter from "./routes/profile";
-import pingRouter from "./routes/ping";
 import photoRouter from "./routes/photo";
 import usersRouter from "./routes/users";
 import fileUploadRouter from "./routes/file-upload";
@@ -31,7 +29,6 @@ require("./libs/passport")(passport);
 
 app.use("/", indexRouter);
 app.use("/profile", profileRouter);
-app.use("/ping", pingRouter);
 app.use("/profile-photo", photoRouter);
 app.use("/users", usersRouter);
 app.use("/files", fileUploadRouter);
